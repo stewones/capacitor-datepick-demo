e0a2152e04c9c37fbb8778029e5b949b924c63b9
Stewan <oi@stewan.io>
2019-07-08 04:10:55 -0300

    feat(): add poc

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { Datepick } from 'capacitor-datepick';
+
+const datepick = new Datepick();
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  selectedDate: string;
+  mode: 'dark' | 'light' = 'dark';
 
   constructor() {}
 
+  ngOnInit() {
+    datepick.darkMode();
+  }
+
+  show() {
+    datepick.present({}).then(date => (this.selectedDate = date.value));
+  }
+
+  darkMode() {
+    this.mode = 'dark';
+    datepick.darkMode();
+  }
+
+  lightMode() {
+    this.mode = 'light';
+    datepick.lightMode();
+  }
 }
